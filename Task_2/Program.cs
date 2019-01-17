c781422541e0ccdff7611899004bad3d2d7654b2
ShemelinPavel <pauls0@mail.ru>
2019-01-17 16:13:52 +0300

    ДЗ №2

---
+++ Task_2/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+
+Shemelin Pavel
+
+2
+
+Реализовать функцию возведения числа a в степень b:
+a. без рекурсии;
+b. рекурсивно;
+c. *рекурсивно, используя свойство чётности степени.
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        public static UInt64 Power_Rec( uint val, uint pow, bool fast, ref uint counter )
+        {
+            if (fast)
+            {
+                counter++;
+                if (pow == 0) return 1;
+                else
+                {
+                    if (pow % 2 == 0) return Power_Rec( val * val, pow/2, fast, ref counter );
+                    else return val * Power_Rec( val, pow - 1, fast, ref counter );
+                }
+            }
+            else
+            {
+                counter++;
+                if (pow == 0) return 1;
+                else return val * Power_Rec( val, pow - 1, fast, ref counter );
+            }
+        }
+
+        public static UInt64 Power( uint val, uint pow, ref uint counter )
+        {
+            UInt64 res = 1;
+            while (pow > 0)
+            {
+                res *= val;
+                pow--;
+                counter++;
+            }
+            return res;
+        }
+
+        static void Main( string[] args )
+        {
+            uint Value = 2;
+            uint Pow = 10;
+            uint counter;
+            UInt64 res;
+
+            counter = 0;
+            res = Power( Value, Pow, ref counter );
+            Console.WriteLine( $"Возведение (без рекурсии) {Value} в степень {Pow}: {res}. Количество шагов: {counter}" );
+
+            counter = 0;
+            res = Power_Rec( Value, Pow, false, ref counter );
+            Console.WriteLine( $"Возведение (рекурсия) {Value} в степень {Pow}: {res}. Количество шагов: {counter}" );
+
+            counter = 0;
+            res = Power_Rec( Value, Pow, true, ref counter );
+            Console.WriteLine( $"Возведение (рекурсия, четность) {Value} в степень {Pow}: {res}. Количество шагов: {counter}" );
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task_3/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+
+Shemelin Pavel
+
+3
+
+Исполнитель «Калькулятор» преобразует целое число, записанное на экране. У исполнителя две команды, каждой присвоен номер: 
+1. Прибавь 1.
+2. Умножь на 2.
+Первая команда увеличивает число на экране на 1, вторая увеличивает его в 2 раза. Определить, сколько существует программ, которые преобразуют число 3 в число 20:
+а. С использованием массива.
+b. *С использованием рекурсии.
+
+*/
+
+using System;
+using System.Collections.Generic;
+
+namespace Task_3
+{
+    class Program
+    {
+        public static int Command_Calc_Dict( int numA, int NumB )
+        {
+            Dictionary<int, int> commandList = new Dictionary<int, int>();
+
+            int res_Plus1 = 0;
+            int res_Mult2 = 0;
+            int i = NumB;
+            while (i >= numA)
+            {
+                commandList.Add( i, 0 );
+
+                res_Plus1 = i + 1;
+                res_Mult2 = i * 2;
+
+                if (res_Plus1 <= NumB) commandList[i] = commandList[i] + commandList[res_Plus1] + ( ( res_Plus1 == NumB ) ? 1 : 0 );
+                if (res_Mult2 <= NumB) commandList[i] = commandList[i] + commandList[res_Mult2] + ( ( res_Mult2 == NumB ) ? 1 : 0 );
+
+                i--;
+            }
+            return ( commandList.Count == 0 ) ? 0 : commandList[numA];
+        }
+
+        public static void Command_Calc_Rec( int numA, int numB, ref int res )
+        {
+            if (numA > numB) return;
+            else if (numA == numB)
+            {
+                res++;
+            }
+            else
+            {
+                Command_Calc_Rec( numA + 1, numB, ref res );
+                Command_Calc_Rec( numA * 2, numB, ref res );
+            }
+        }
+
+        static void Main( string[] args )
+        {
+            Console.BufferWidth = 100;
+            Console.WindowWidth = 100;
+
+            int num1 = 3;
+            int num2 = 20;
+            int res = 0;
+
+            res = Command_Calc_Dict( num1, num2 );
+            Console.WriteLine( $"Количество программ преобразования числа: {num1} в число {num2} через массив (словарь): {res}" );
+
+            res = 0;
+            Command_Calc_Rec( num1, num2, ref res );
+            Console.WriteLine( $"Количество программ преобразования числа: {num1} в число {num2} через рекурсию: {res}" );
+
+            Console.ReadKey();
+        }
+    }
+}
